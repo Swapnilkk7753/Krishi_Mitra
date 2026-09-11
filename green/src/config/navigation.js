b95c87d22c68c69c0e1d335e0228a8c1780e5d35
SWAPNIL KESHARWANI <swapnilkesharwani3206@gmail.com>
2026-09-11 19:43:00 +0530

    Add files via upload

---
+++ green/src/config/navigation.js
@@ -0,0 +1,10 @@
+import { Cardiogram01Icon, ChartLineData01Icon, ChatBotIcon, DashboardSquare01Icon, Notification01Icon, Plant01Icon } from "@hugeicons/core-free-icons";
+
+export const navigation = [
+    { name : "Dashboard", url : '/user/dashboard', icon : DashboardSquare01Icon },
+    { name : "Notification", url : '/users/notification', icon : Notification01Icon },
+    { name : "Plant Health", url : '/user/field', icon : Plant01Icon },
+    { name : "Chat", url : '/users/chat', icon : ChatBotIcon },
+    // { name : "Analytics", url : '/users/analytics', icon : ChartLineData01Icon },
+    // { name : "Hardware", url : '/users/logout', icon : Cardiogram01Icon },
+]
